c61854f438de511d573f6993bcd969f6003d275e
emanherawy <eman.herawy@gmail.com>
2021-06-29 23:57:25 +0200

    token and stake documentation

--- contracts/StartFiToken.sol
+++ contracts/StartFiToken.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 pragma solidity >=0.8.0;
-
+pragma experimental SMTChecker;
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
-
+/**
+ * @author Eman Herawy, StartFi Team
+ *@title  Start FiToken
+ * desc A Startfi Utiltiy token
+ */
 contract StartFiToken is ERC20PresetFixedSupply{
     constructor(string memory name,
         string memory symbol,
