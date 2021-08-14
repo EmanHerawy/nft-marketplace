2cd5ae50787484479a833e2a3d310a7a2b5c9d40
emanherawy <eman.herawy@gmail.com>
2021-08-14 05:04:53 +0200

    update contract

--- contracts/StartFiTokenDistribution.sol
+++ contracts/StartFiTokenDistribution.sol
@@ -16,9 +16,8 @@ import "./interface/IERC20.sol";
 contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
   
   /******************************************* decalrations go here ********************************************************* */
- 	uint256 constant public decimals = 1 ether;
 	
-	address[] public tokenOwners ; /* Tracks distributions mapping (iterable) */
+	address[2] public tokenOwners ; /* Tracks distributions mapping (iterable) */ 
 	uint256 public TGEDate = 0; /* Date From where the distribution starts (TGE) */
 	
 	uint256 constant public month = 30 days;
@@ -26,7 +25,7 @@ contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
 	uint256 public lastDateDistribution = 0;
   
 	
-	mapping(address => DistributionStep[]) public distributions; /* Distribution object */
+	mapping(address => DistributionStep[]) private _distributions; /* Distribution object */
 	
 	address public erc20;
 
@@ -45,8 +44,8 @@ contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
  /******************************************* constructor goes here ********************************************************* */
 
  	constructor(address _erc20, uint256 _time,address _owner){
-		_erc20=erc20;
-		TGEDate = _time;
+		erc20=_erc20;
+		TGEDate =	_time<block.timestamp?block.timestamp:_time;
 		transferOwnership(_owner);
 		// /* Seed */
 		// setInitialDistribution(0xc97, 3000000, 0 /* No Lock */);
@@ -115,6 +114,15 @@ contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
 		// setInitialDistribution(0x20, 2500000, year.add(3 * month)); /* After 3 Month */
 		// setInitialDistribution(0x20, 2500000, year.add(6 * month)); /* After 6 Month */
 		// setInitialDistribution(0x20, 2500000, year.add(9 * month)); /* After 9 Month */
+		
+		// test 
+		//tokenOwners.push(_address);
+		_setInitialDistribution(msg.sender, 10, 0 /* No Lock */);
+		_setInitialDistribution(_owner, 10, 0 /* No Lock */);
+		_setInitialDistribution(msg.sender, 50, 10 * month); /* After 1 Month */
+		_setInitialDistribution(_owner, 100, 10 * month); /* After 1 Month */
+		tokenOwners[0]=msg.sender;
+		tokenOwners[1]=_owner;
 	}
 
   /******************************************* modifiers go here ********************************************************* */
@@ -129,6 +137,18 @@ contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
 
   /******************************************* read state functions go here ********************************************************* */
 
+function getBeneficiaryPoolLength(address beneficary) view public returns (uint256 arrayLneght) {
+	return _distributions[beneficary].length;
+}
+function getBeneficiaryPoolInfo(address beneficary, uint256 index) view external returns (	uint256 amountAllocated,
+		uint256 currentAllocated,
+		uint256 unlockDay,
+		uint256 amountSent) {
+			amountAllocated= _distributions[beneficary][index]. amountAllocated;
+			currentAllocated=_distributions[beneficary][index].  currentAllocated;
+			unlockDay= _distributions[beneficary][index]. unlockDay;
+			amountSent= _distributions[beneficary][index].amountSent;
+}
   /******************************************* state functions go here ********************************************************* */
 
 
@@ -163,8 +183,7 @@ contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
 	 */
 
 	function triggerTokenSend() external whenNotPaused nonReentrant {
-		/* Require TGE Date already been set */
-		require(TGEDate != 0, "TGE date not set yet");
+	
 		/* TGE has not started */
 		require(block.timestamp > TGEDate, "TGE still has not started");
 		/* Test that the call be only done once per day */
@@ -173,7 +192,7 @@ contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
 		/* Go thru all tokenOwners */
 		for(uint i = 0; i < tokenOwners.length; i++) {
 			/* Get Address Distribution */
-			DistributionStep[] memory d = distributions[tokenOwners[i]];
+			DistributionStep[] memory d = _distributions[tokenOwners[i]];
 			/* Go thru all distributions array */
 			for(uint j = 0; j < d.length; j++){
 				if( (block.timestamp-TGEDate > d[j].unlockDay) /* Verify if unlockDay has passed */
@@ -181,33 +200,20 @@ contract StartFiTokenDistribution is  Ownable ,Pausable,ReentrancyGuard {
 				){
 					uint256 sendingAmount;
 					sendingAmount = d[j].currentAllocated;
-					distributions[tokenOwners[i]][j].currentAllocated = distributions[tokenOwners[i]][j].currentAllocated-sendingAmount;
-					distributions[tokenOwners[i]][j].amountSent = distributions[tokenOwners[i]][j].amountSent+sendingAmount;
+					_distributions[tokenOwners[i]][j].currentAllocated = _distributions[tokenOwners[i]][j].currentAllocated-sendingAmount;
+					_distributions[tokenOwners[i]][j].amountSent = _distributions[tokenOwners[i]][j].amountSent+sendingAmount;
 					require(IERC20(erc20).transfer(tokenOwners[i], sendingAmount));
 				}
 			}
 		}   
 	}
 
-	function setInitialDistribution(address _address, uint256 _tokenAmount, uint256 _unlockDays) internal onlyOwner whenNotPaused {
-		/* Add tokenOwner to Eachable Mapping */
-		bool isAddressPresent = false;
-
-		/* Verify if tokenOwner was already added */
-		for(uint i = 0; i < tokenOwners.length; i++) {
-			if(tokenOwners[i] == _address){
-				isAddressPresent = true;
-			}
-		}
+	function _setInitialDistribution(address _address, uint256 _tokenAmount, uint256 _unlockDays) private  {
+	
 		/* Create DistributionStep Object */
-		DistributionStep memory distributionStep = DistributionStep(_tokenAmount * decimals, _tokenAmount * decimals, _unlockDays, 0);
+		DistributionStep memory distributionStep = DistributionStep(_tokenAmount , _tokenAmount , _unlockDays, 0);
 		/* Attach */
-		distributions[_address].push(distributionStep);
-
-		/* If Address not present in array of iterable token owners */
-		if(!isAddressPresent){
-			tokenOwners.push(_address);
-		}
+		_distributions[_address].push(distributionStep);
 
 	}
 }
