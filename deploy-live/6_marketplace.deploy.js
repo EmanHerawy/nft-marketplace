67c291a1b52f8a8e8e7d8ad0818907da500de482
emanherawy <eman.herawy@gmail.com>
2021-11-25 16:47:28 +0200

    go to main net on ethereum and heco

---
+++ deploy-live/6_marketplace.deploy.js
@@ -0,0 +1,83 @@
+const { hexlify, formatBytes32String } = require('ethers/lib/utils')
+const contracts = require('../deployments/computedAddreses.json')
+const expandTo18Decimals = require('../test/shared/utilities').expandTo18Decimals;
+
+// deploy/00_deploy_my_contract.js
+module.exports = async ({ deployments, getNamedAccounts }) => {
+  const StartFiMarketPlace = await ethers.getContractFactory('StartFiMarketPlace')
+  let StartfiCreate2Deployer = await ethers.getContractFactory('StartfiCreate2Deployer')
+   const { get, execute } = deployments
+  const { deployer } = await getNamedAccounts()
+  const _usdCap = expandTo18Decimals(20000);
+  const _stfiCap = expandTo18Decimals(200000);
+  const _stfiUsdt = expandTo18Decimals(10);
+  let create2Deployer = await get('StartfiCreate2Deployer')
+  // let stfi_token= await get('StartFiToken');
+  //   let stakesContract= await get('StartFiStakes');
+  //    let nft_token= await get('StartFiRoyaltyNFT');
+  //   let nftoken= await get('StartFiRoyaltyNFT');
+  //   let startFi_reputation= await get('StartFiReputation');
+
+  const constructorArgs = ['StartFi Market', contracts.stfiToken, contracts.staking, deployer , _usdCap , _stfiCap , _stfiUsdt ]
+  // const constructorArgs = ["StartFi Market",stfi_token.address,stakesContract.address, startFi_reputation.address,deployer]
+
+  // 0x19550457F532A47f8B64e1246563e9013DF20260
+  // let factoryAddress = "0x19550457F532A47f8B64e1246563e9013DF20260"; // if localhost , deploy first !
+  let factoryAddress = create2Deployer.address //'Tp be added' // if localhost , deploy first !
+
+  // if (network.name == `hardhat` || network.name == `localhost`) {
+  //   const factoryDeployed = await deploy('AnyNFTCreate2Deployer', {
+  //     from: deployer,
+  //     args: [],
+  //     log: true,
+  //   })
+  //   factoryAddress = factoryDeployed.address
+
+  //   console.log(factoryAddress, 'factoryAddress')
+  // }
+  const constructorTypes = ['string', 'address', 'address', 'address', 'uint256', 'uint256', 'uint256']
+  // console.log(StartFiMarketPlace.bytecode,'StartFiMarketPlace.bytecode');
+  const constructor = encodeParam(constructorTypes, constructorArgs).slice(2)
+  const bytecode = `${StartFiMarketPlace.bytecode}${constructor}`
+  // const bytecode = `${optimizedbytecode}${constructor}`
+  const salt = formatBytes32String('Startfi2021')
+  // console.log({bytecode});
+  // console.log({salt});
+  // encodes parameter to pass as contract argument
+  function encodeParam(dataType, data) {
+    const abiCoder = ethers.utils.defaultAbiCoder
+    return abiCoder.encode(dataType, data)
+  }
+
+  function buildCreate2Address(creatorAddress, saltHex, byteCode) {
+    return `0x${ethers.utils
+      .keccak256(
+        `0x${['ff', creatorAddress, saltHex, ethers.utils.keccak256(byteCode)]
+          .map((x) => x.replace(/0x/, ''))
+          .join('')}`
+      )
+      .slice(-40)}`.toLowerCase()
+  }
+
+  // returns true if contract is deployed on-chain
+  async function isContract(address) {
+    const code = await ethers.provider.getCode(address)
+    return code.slice(2).length > 0
+  }
+
+  // First see if already deployed
+  const computedAddr = buildCreate2Address(factoryAddress, hexlify(salt), bytecode)
+  console.log(computedAddr, 'computedAddr')
+  const isDeployed = await isContract(computedAddr)
+  if (!isDeployed) {
+    const factory = await StartfiCreate2Deployer.attach(factoryAddress)
+    const result = await (await factory.deploy(0, salt, bytecode)).wait()
+    // const addr = result.events[0].args.newAddress.toLowerCase()
+    console.log({ result })
+    // console.log({ addr })
+  }
+
+ 
+}
+
+module.exports.tags = ['StartFiMarketPlace']
